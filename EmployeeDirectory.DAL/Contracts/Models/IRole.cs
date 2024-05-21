5829a9b870f1d945eedfd6de77c38d4fcd890ead
Lav Kumar <lav.k@Technovert.com>
2024-05-21 16:01:28 +0530

    added updated code

---
+++ EmployeeDirectory.DAL/Contracts/Models/IRole.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeDirectory.DAL.Contracts.Models
+{
+    public interface IRole
+    {
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        public string Location { get; set; }
+
+        public string Id { get; set; }
+    }
+}
---
+++ EmployeeDirectory.DAL/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeDirectory.DAL.Contracts.Models;
+
+namespace EmployeeDirectory.DAL.Models
+{
+    public class Role : IRole
+    {
+        public required string Name { get; set; }
+
+        public required string Department { get; set; }
+
+        public required string Location { get; set; }
+
+        public required string Id { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
+
